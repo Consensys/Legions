a86e8cfd50a9fd861fddb6fdf62762f724601586
Daniel Luca <lucadanielcostin@gmail.com>
2020-03-31 19:06:54 +0300

    Add `ens info`.

--- legions/commands/more/ens.py
+++ legions/commands/more/ens.py
@@ -29,10 +29,31 @@ class ens:
     @argument("value", description="name to be converted to an address")
     def toAddress(self, value: str) -> str:
         """
-        Converts a ens name to an address
+        Converts a ENS name to an address
         """
 
         try:
             cprint("Address of {}: {}".format(value, self.ns.address(value)))
         except Exception as e:
             cprint("Failed to convert {}: {}".format(value, e), "yellow")
+
+    @command("info")
+    @argument("name", description="name to get information about")
+    def info(self, name: str) -> str:
+        """
+        Get info about an ENS name
+        """
+
+        cprint("Information about '{}'".format(name))
+        cprint("Valid name: {}".format(self.ns.is_valid_name(name)))
+
+        if self.ns.is_valid_name(name):
+            cprint("Namehash: {}".format(self.ns.namehash(name).hex()))
+
+            resolver = self.ns.resolver(name)
+            if resolver is not None:
+                cprint("Resolver: {}".format(resolver.address))
+                cprint("Owner: {}".format(self.ns.owner(name)))
+            else:
+                cprint("Is not registered")
+
